79ad7640d97b2053629f82ea66ff640d4fd31551
prathix <ritukhanna1717@gmail.com>
2024-05-29 20:52:58 +0200

    added first javscript, removed eccessive code

--- portfolio/portfoliov3/static/portfoliov3/script.js
+++ portfolio/portfoliov3/static/portfoliov3/script.js
@@ -1,5 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.querySelectorAll(".categories").forEach(function(category){
-        
+document.addEventListener("DOMContentLoaded", function() {                              /* function for making the starting ring and logo disappear after you click one of the categories */
+    document.querySelectorAll(".category").forEach(function(category) {
+        category.addEventListener("click", function() {
+            let ring = document.querySelector(".ring");
+            let logo = document.querySelector("#logo");
+            ring.style.visibility = "hidden";
+            logo.style.visibility = "hidden";
+        });
     });
 });
