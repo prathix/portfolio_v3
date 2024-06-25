8d742e4ef19fdc7335a9a827283393aa5414ccf8
prathix <ritukhanna1717@gmail.com>
2024-06-25 22:59:03 +0200

    finished starting page animations

--- portfolio/portfoliov3/static/portfoliov3/script.js
+++ portfolio/portfoliov3/static/portfoliov3/script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {     /* main function for starting the various animations */
     let logo = document.querySelector("#logo");
-    let logo_class = document.querySelector(".logo");
     let ring = document.querySelector(".ring");
     logo.style.animationPlayState = 'paused';                  
     document.querySelectorAll(".category").forEach(function(category) {
@@ -8,6 +7,32 @@ document.addEventListener("DOMContentLoaded", function() {     /* main function 
             changeSpeed();
         });
     });
+
+    function clicked(categ) {                                                 /* function for changing the pages css when clicked */
+        let element = document.querySelector(categ);
+        if (element) {
+            element.addEventListener("click", function() {
+                setTimeout(function() {
+                    let body = document.querySelector("body");
+                    if (categ === "#game_dev") {
+                        body.style.backgroundColor = "white";
+                    } else if (categ === "#ai_dev") {
+                        body.style.backgroundColor = "blue";
+                    } else if (categ === "#web_dev") {
+                        body.style.backgroundColor = "red";
+                    } else if (categ === "#miscellaneous") {
+                        body.style.backgroundColor = "brown";
+                    }
+                }, 2000);
+            });
+        }
+    }
+
+    clicked("#game_dev");
+    clicked("#ai_dev");
+    clicked("#web_dev");
+    clicked("#miscellaneous");
+
     function changeSpeed() {                                      /* function for changing the duration of the rotation */
         let duration = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--animation-duration'));
         decreaseDuration();
@@ -20,12 +45,6 @@ document.addEventListener("DOMContentLoaded", function() {     /* main function 
                     ring.classList.toggle('bigger');
                     decreaseDuration();
                 }
-            else{
-                setTimeout(function() {
-                    ring.style.visibility = 'hidden';
-                    logo_class.classList.toggle('logo_animation');
-                }, 3000);
-            }
         }
     }
 });
