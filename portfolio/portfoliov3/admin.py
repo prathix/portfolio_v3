44ee9e22786e96549fffbfc6ebd5ed0a1f576167
prathix <ritukhanna1717@gmail.com>
2024-06-28 22:59:59 +0200

    finished backend part

--- portfolio/portfoliov3/admin.py
+++ portfolio/portfoliov3/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Projects
 
-# Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'short_description')
+    list_filter = ('category',)
+    search_fields = ('title', 'short_description')
+
+admin.site.register(Projects, ProjectAdmin)
--- portfolio/portfoliov3/models.py
+++ portfolio/portfoliov3/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 class Projects(models.Model):
+    CATEGORY_CHOICES = [
+        ('web', 'Web Development'),
+        ('ai', 'AI Development'),
+        ('game', 'Game Development'),
+        ('miscellaneous', 'miscellaneous'),
+        # Add more categories as needed
+    ]
+
+    category = models.CharField(max_length=15, choices=CATEGORY_CHOICES)
+
     title = models.CharField(max_length = 32)
     short_description = models.CharField(max_length = 250)
     long_description = models.CharField(max_length = 1024)
