94b09eb2dbec089e7507a85a3e651ba2a95bef19
prathix <ritukhanna1717@gmail.com>
2024-09-18 21:38:45 +0200

    aggiunto pulsante, non ci reisco ancora a cliccare

--- portfolio/portfoliov3/models.py
+++ portfolio/portfoliov3/models.py
@@ -13,5 +13,5 @@ class Projects(models.Model):
 
     title = models.CharField(max_length = 32)
     short_description = models.CharField(max_length = 250)
-    long_description = models.CharField(max_length = 1024)
+    link = models.CharField(max_length = 1024)
     image = models.ImageField(upload_to='portfoliov3/images/')
